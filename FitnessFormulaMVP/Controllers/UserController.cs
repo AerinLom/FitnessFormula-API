499dc143be482300ced8162867deea5fd9f89a9e
AerinLom <aerinlom1@gmail.com>
2024-06-19 23:55:24 +0200

    Implemented Landing Page view(MKs brilliance)

---
+++ FitnessFormulaMVP/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessFormulaMVP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+
+}
